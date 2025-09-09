33f7c2da7b0353d12025f00ab9536174b10befb4
Zahra <zahra@zahras-mbp.mynetworksettings.com>
2025-09-09 14:35:51 -0400

    adding classes.js and functions.js

---
+++ classes.js
@@ -0,0 +1,12 @@
+class Cat {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const myFirstCat = new Cat('Fluffy', 5)
+console.log(myFirstCat)
+
+const mySecondCat = new Cat('Bella', 3)
+console.log(mySecondCat.age)
---
+++ functions.js
@@ -0,0 +1,18 @@
+function myFirstFunction() {
+    console.log("This is my first function!");
+}
+
+myFirstFunction()
+
+function mySecondFunction(name) {
+    console.log('Hi, ' + name + '!');
+}
+
+mySecondFunction('Zahra')
+mySecondFunction('Jamal')
+
+function addTewoNumbers(x, y) {
+    return x + y
+}
+
+console.log(addTewoNumbers(2, 3))
