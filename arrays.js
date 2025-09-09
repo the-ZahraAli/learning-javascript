b3371f1fb8ee0cd35a565a90d579c82b77ca6cd0
Zahra <zahra@zahras-mbp.mynetworksettings.com>
2025-09-09 13:56:09 -0400

    adding loops.js and arrays.js

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let fruits = ['apple', 'orange', 'pear']
+console.log(fruits)
+console.log(fruits[0])
+
+console.log(fruits.length)
+
+fruits.push('grape')
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+
+fruits.splice(1,0, 'banana')
+console.log(fruits)
---
+++ loops.js
@@ -0,0 +1,16 @@
+// For looop
+
+// first part - creates a counter variable
+// second part - sets the condition in which the loop will execute
+// third part - increments the counter variable
+
+for(x = 1; x <5; x++) {
+    console.log("x is: " + x)
+}
+
+let y = "foo"
+
+for(x = 1; x < 5; x++) {
+    y = y + "bar"
+    console.log(y)
+}
