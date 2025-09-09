70bef188988cd46f8743fe4bd8f0fbdde0acb82d
Zahra <zahra@zahras-mbp.mynetworksettings.com>
2025-09-09 12:08:05 -0400

    prompt package installed. what time is it? code

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')()
+
 let x = 4
 let y = 5
 
@@ -9,4 +11,17 @@ if(x > y) {
     console.log("x is greater than y")
 } else {
     console.log("Nope! x is not greater than y")
+}
+
+
+// A program to determine whhether its time to get up
+
+let hour = prompt("What time is it? ")
+
+if(hour < 4) {
+    console.log("It's the middle of the night. Keep sleeping!")
+} else if(hour > 4 && hour < 7){
+    console.log("You can sleep a little longer.")
+} else {
+    console.log("Why are you still sleeping? Get up!")
 }
