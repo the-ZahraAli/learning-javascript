899f20a6cfcd445e92db27fa73e442725263fd45
Zahra <zahra@zahras-mbp.mynetworksettings.com>
2025-09-10 10:13:52 -0400

    Classes lessons

---
+++ class-call.js
@@ -0,0 +1,5 @@
+const Dog = require('./classes2.js')
+
+let myFirstDog = new Dog()
+myFirstDog.woof()
+
---
+++ classes2.js
@@ -0,0 +1,11 @@
+const colors = require('colors');
+class Dog {
+    constructor() {
+    }
+
+    woof() {
+        console.log('Woof!'.red);
+    }
+}
+
+module.exports = Dog;
